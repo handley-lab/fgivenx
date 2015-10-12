735055ae622a450b964e46a8cc57030db43cc745
Will Handley <wh260@cam.ac.uk>
2015-10-12 23:39:53 +0100

    Much faster kde
    * Using statsmodels (which may be slightly difficult to install) makes
      for a very fast kde (using fft's)
    * In addition, it makes sense to compute all of the slices ahead of time
      (otherwise we become dominated by calling classes)
    
    To investigate:
    - Do we need to use statsmodels? It was a little difficult to install.
    - It would be better to have a more robust method for determining
      bandwidth

--- fgivenx.py
+++ fgivenx.py
@@ -2,6 +2,8 @@ from scipy.interpolate import interp1d
 import numpy as np
 import matplotlib.pyplot as plt
 
+import statsmodels.nonparametric.kde as KDETOOLS
+
 
 # Definition of pi
 pi = 4*np.arctan(1)
@@ -21,13 +23,13 @@ def randomSample():
     x = np.linspace(-pi,pi,n)
     for i in range(1,n-1):
         x[i]+=np.random.randn()*0.2
-    y = np.cos(-x) + np.random.randn(n)*0.2
+    y = np.random.choice([-1,1])*np.cos(-x) + np.random.randn(n)*0.2
     return LinearSample(x,y)
 
 # Compute n samples from the above
 def randomSamples(N=100):
     samples =[]
-    for k in range(1,N,1):
+    for k in range(0,N,1):
         samples.append(randomSample())
     return np.array(samples)
 
@@ -35,15 +37,17 @@ def randomSamples(N=100):
 
 # Compute a slice of the function at a valid x
 def slice(samples,x):
-    slice_list = list()
-    for sample in samples:
-        val = sample.f(x)
-        slice_list.append(val)
-    return np.array(slice_list)
+    return np.array([sample.f(x) for sample in samples])
 
 # Compute a kde estimator
-def kde(data,b=0.1):
-    return KernelDensity(bandwidth=b).fit(np.transpose([data]))
+#def kde(data,b=0.1):
+#    return KernelDensity(bandwidth=b).fit(np.transpose([data]))
+
+def compute_kernel(data):
+    return KDETOOLS.KDEUnivariate(data).fit()
+
+def pdf(x,kernel):
+    return kernel.evaluate(x)
 
 
 
@@ -52,7 +56,7 @@ def kde(data,b=0.1):
 # Generate the samples
 # --------------------
 print "Generating samples"
-nsamp=5000
+nsamp=20000
 samples = randomSamples(nsamp)
 data = slice(samples,0)
 
@@ -77,15 +81,21 @@ from sklearn.grid_search import GridSearchCV
 #b = grid.best_estimator_.bandwidth + 0.0
 #print("best bandwidth: {0}".format(b))
 
+xvals = np.linspace(pi/2,pi,20)
 
+print "computing slices"
+slices = np.array([sample.f(xvals) for sample in samples]).T
+xnew = np.linspace(-1.5,1.5,1000)
 
-for x in [0,0.3,0.6,0.9,1.2]:
-    print x
-    data = slice(samples,x)
-    kernel = kde(data,0.1)
-    xnew = np.linspace(-1.5,1.5,1000)
-    ynew = np.exp(kernel.score_samples(np.transpose([xnew])))
-    plt.plot(xnew,ynew)
+for s in slices :
+    print "computing kernel"
+    kernel = KDETOOLS.KDEUnivariate(s)
+    print "fitting kernel"
+    kernel.fit()
+    #ynew = kernel.evaluate(xnew)
+    print "plotting"
+    plt.plot(kernel.support,kernel.density)
+    print "---------------------------"
 
 plt.show()
 
@@ -98,47 +108,5 @@ sys.exit(0)
 #    ws.append(1.0/nsamp)
 #
 #plt.hist(data,weights=ws,normed=True)
-
-
-def compute_kde(samples,x):
-    return gaussian_kde(slice(samples,x)) 
-
-
-
-
-
-
-# Plot the samples
-# ----------------
-print "Plotting histograms"
-
-xnew = np.linspace(0.5,1.5,1000)
-for xi in [0.0,0.1,0.2,0.3]:
-    print xi
-    kernel = compute_kde(samples,xi)
-    print "plotting"
-    plt.plot(xnew,kernel(xnew), '-')
-
-
-plt.show()
-import sys
-sys.exit(0)
-
-plt.hist(slice(samples,0))
-plt.hist(slice(samples,1))
-plt.hist(slice(samples,2))
-plt.hist(slice(samples,3))
-plt.show()
-
-plt.clf() # clear the figure
-
-xnew = np.linspace(-pi,pi,100)
-plt.plot(xnew,np.cos(xnew), '-')
-
-for sample in samples:
-    plt.plot(xnew,sample.f(xnew), '-')
-
-plt.show()
-
 
 
