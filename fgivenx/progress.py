da58c173719bd73e6e82e9e93046cae219a1a969
Will Handley <wh260@cam.ac.uk>
2015-10-18 11:19:48 +0100

    Updated progress bar to automatically re-size

--- fgivenx/progress.py
+++ fgivenx/progress.py
@@ -3,15 +3,12 @@ import os
 
 class ProgressBar(object):
     percent = -1
-    bar_length = 40
     i=0.0
     n=0
     message=" "
     def __init__(self,n,message=" "):
         self.n=n
         self.message=message
-        rows, columns = os.popen('stty size', 'r').read().split()
-        self.bar_length= int(columns) - len(message) - 15
 
 
     def __call__(self):
@@ -25,11 +22,16 @@ class ProgressBar(object):
             progress = 1
             status = "Done\r\n\r"
 
-        block = int(round(self.bar_length*progress))
         percent = round(progress*1000)/10
+
         if percent > self.percent or last:
             self.percent = percent
-            text = "\r" + self.message + " [{0}] {1}% {2} ".format( "#"*block + "-"*(self.bar_length-block), percent, status)
+
+            rows, columns = os.popen('stty size', 'r').read().split()
+            bar_length= int(columns) - len(self.message) - 15
+            block = int(round(bar_length*progress))
+
+            text = "\r" + self.message + " [{0}] {1}% {2} ".format( "#"*block + "-"*(bar_length-block), percent, status)
             sys.stdout.write(text)
             sys.stdout.flush()
 
