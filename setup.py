8efcb75eb39ce09ed2ebd764879d8c8d6f444046
Will Handley <wh260@cam.ac.uk>
2017-08-17 15:26:15 +0100

    Added a setup.py to package the distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='fgivenx',
+      version='1.0',
+      description='Functional Posterior Plotter',
+      author='Will Handley',
+      author_email='wh260@cam.ac.uk',
+      url=None,
+      packages=['fgivenx'],
+      )
+
