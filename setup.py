0832bbcf58f5b44e8ef7ecb2c7f8720bdb22184a
ejhigson <ejhigson@gmail.com>
2018-04-09 12:34:37 +0100

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, ImportError):
     long_description = ''
 
 setup(name='fgivenx',
-      version='1.1.3',
+      version='1.1.4',
       author='Will Handley',
       author_email='wh260@cam.ac.uk',
       url='https://github.com/williamjameshandley/fgivenx',
