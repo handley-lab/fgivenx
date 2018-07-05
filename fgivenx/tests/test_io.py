973e42b50bc248888edc10d4e127de4a22da495e
Will Handley <wh260@cam.ac.uk>
2018-07-05 13:47:31 +0100

    Updated .travis to only test python 2.7 and 3.6

--- fgivenx/tests/test_io.py
+++ fgivenx/tests/test_io.py
@@ -1,14 +1,23 @@
 import numpy
 import pytest
 import os
-from numpy.testing import assert_allclose
-
 import fgivenx.io
+import pytest
 
-def test_cache_dir_creation():
+def test_Cache_dir_creation():
+    """ Check that cache can create directories """
     dirname = '.test_cache'
     root = os.path.join(dirname,'test_root')
     assert(not os.path.isdir(dirname))
     cache = fgivenx.io.Cache(root)
     assert(os.path.isdir(dirname))
     os.rmdir(dirname)
+
+def test_Cache_load():
+    """ Check that cache """
+
+    root = '.test_cache/test_root'
+    cache = fgivenx.io.Cache(root)
+    with pytest.raises(fgivenx.io.CacheMissing):
+        cache.load()
+    os.rmdir('.test_cache')
