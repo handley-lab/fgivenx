cbe1b789a8e7c827d36e4c53148067d471dfaa6e
Will Handley <wh260@cam.ac.uk>
2023-04-04 22:23:48 +0100

    Updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,12 +55,6 @@ extensions = [
     'numpydoc',
 ]
 
-intersphinx_mapping = {
-        'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-        'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
-        'matplotlib': ('http://matplotlib.org/', None),
-        }
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -78,17 +72,22 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path .
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
 
+autosummary_generate = True
+
+# -- Options for numpydoc -------------------------------------------------
+numpydoc_show_inherited_class_members = False
+numpydoc_show_class_members = False 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -105,7 +104,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -201,7 +200,11 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+        'numpy':('https://numpy.org/doc/stable', None),
+        'scipy':('https://docs.scipy.org/doc/scipy', None),
+        'matplotlib':('https://matplotlib.org/stable', None),
+        }
 
 # -- Options for todo extension ----------------------------------------------
 
--- docs/source/fgivenx.rst
+++ docs/source/fgivenx.rst
@@ -1,69 +1,66 @@
 fgivenx package
 ===============
 
-Module contents
----------------
-
 .. automodule:: fgivenx
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 Submodules
 ----------
-
-fgivenx.drivers module
-----------------------
-
-.. automodule:: fgivenx.drivers
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 fgivenx.dkl module
 ------------------
 
 .. automodule:: fgivenx.dkl
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+fgivenx.drivers module
+----------------------
+
+.. automodule:: fgivenx.drivers
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 fgivenx.io module
 -----------------
 
 .. automodule:: fgivenx.io
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 fgivenx.mass module
 -------------------
 
 .. automodule:: fgivenx.mass
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 fgivenx.parallel module
 -----------------------
 
 .. automodule:: fgivenx.parallel
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 fgivenx.plot module
 -------------------
 
 .. automodule:: fgivenx.plot
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 fgivenx.samples module
 ----------------------
 
 .. automodule:: fgivenx.samples
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
